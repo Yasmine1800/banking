dbffa169a6163e0071520c85949881256e315b8d
Yasmine1800 <yasminelafhim@gmail.com>
2024-05-25 15:00:21 +0100

    Implement the complete layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,23 @@
-export default function RootLayout({children , 
+import MobileNav from "@/components/MobileNav";
+import Sidebar from "@/components/Sidebar";
+import Image from "next/image";
 
-}: Readonly<{
-    children: React.ReactNode;
-}>) {
-        return (
-            <main>
-               SIDEBAR
-               {children}
-            </main>
-        );
-    }
+export default function RootLayout({children}: Readonly<{children: React.ReactNode;}>) {
+
+    const loggedIn = { fistName: "Yasmine", lastName: "Gorgeous"}
+
+    return (
+        <main className="flex h-screen w-screen font-inter">
+            <Sidebar/>
+            <div className="flex size-full flex-col">
+                <div className="root-layout">
+                    <Image src="/icons/logo.svg" width={30} height={30} alt="logo"/>
+                    <div>
+                        <MobileNav user={loggedIn}/>
+                    </div>
+                </div>
+            {children}
+            </div>
+        </main>
+    );
+}
